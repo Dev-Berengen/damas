b633e4661481d23f02773c9d0d04d0535bd8cda8
Dev-Berengen <dev.berengen@gmail.com>
2025-10-15 22:45:34 +0000

    0.024

--- data.js
+++ data.js
@@ -6,147 +6,38 @@ export const data = [
     prix: "6.000 Fcfa",
     // Description de 30 mots
     description:
-      "L’humeur hautaine t toutes les portes évêque et le laissèrent passer. Il fit quelques pas et se trouva dans une immense salle gothique extrêmement sombre, et toute lambrissée de chêne noir ; à l’exception d’une seule, les fenêtres en ogive avaient été murées avec des briques.",
-    liens: "homme",
+      "",
+    liens: "",
   },
   {
     id: 2,
     img: "./assets/BOSE7000.jpg",
-    nom: "Bosse 700",
-    prix: 7,
+    nom: "BOSE 700",
+    prix: "9.000 Fcfa",
     // Description de 30 mots
-    description:
-      "L’humeur hautaine de Julien fut choquée de l’insolence des laquais. Il se mit à parcourir les dortoirs de l’antique abbaye, secouant toutes les portes qu’il rencontrait. Une fort petite céda à ses efforts, et il se trouva dans une cellule au milieu des valets de chambre de Monseigneur, en habits noirs et la chaîne au cou. À son air pressé ces messieurs le crurent mandé par l’évêque et le laissèrent passer. Il fit quelques pas et se trouva dans une immense salle gothique extrêmement sombre, et toute lambrissée de chêne noir ; à l’exception d’une seule, les fenêtres en ogive avaient été murées avec des briques.",
-    categorie: "homme",
+    description:"",
+    categorie: "",
   },
   {
     id: 3,
-    img: "./assets/PB_10000mah.jpg",
-    nom: "POWER BANK | 10.000mah",
-    prix: 15,
+    img: "./assets/SONY.jpg",
+    nom: "CASQUE SONY | WB910",
+    prix:  "6.000 Fcfa",
     // Description de 30 mots
     description:
-      "L’humeur hautaine de Julien fut choquée de l’insolence des laquais. Il se mit à parcourir les dortoirs de l’antique abbaye, secouant toutes les portes qu’il rencontrait. Une fort petite céda à ses efforts, et il se trouva dans une cellule au milieu des valets de chambre de Monseigneur, en habits noirs et la chaîne au cou. À son air pressé ces messieurs le crurent mandé par l’évêque et le laissèrent passer. Il fit quelques pas et se trouva dans une immense salle gothique extrêmement sombre, et toute lambrissée de chêne noir ; à l’exception d’une seule, les fenêtres en ogive avaient été murées avec des briques.",
-    categorie: "homme",
+      "",
+    categorie: "",
   },
   {
     id: 4,
-    img: "./assets/USB.jpg",
-    nom: "Man's hat swag",
-    prix: 10,
+    img: "./assets/K73.jpg",
+    nom: "ECOUTEUR SAMSUNG | K73",
+    prix:  "5.000 Fcfa",
     // Description de 30 mots
     description:
-      "L’humeur hautaine de Julien fut choqué Il se mit à parcourir les dortoirs de l’antique abbaye, secouant toutes les portes qu’il rencontrait. Une fort petite céda à ses efforts, et il se trouva dans une cellule au milieu des valets de chambre de Monseigneur, en habits noirs et la chaîne au cou. À son air pressé ces messieurs le crurent mandé par l’évêque et le laissèrent passer. Il fit quelques pas et se trouva dans une immense salle gothique extrêmement sombre, et toute lambrissée de chêne noir ; à l’exception d’une seule, les fenêtres en ogive avaient été murées avec des briques.",
-    categorie: "homme",
+      "",
+    categorie: "",
   },
-  {
-    id: 5,
-    img: "./assets/USB_32GB.jpg",
-    nom: "Chapeau nike air",
-    prix: 40,
-    // Description de 30 mots
-    description:
-      "L’humeur hautaine de Julien fut choquée de l’insolence des laquais. Il se mit à parcourir les dortoirs de l’antique abbaye, secouant toutes les portes qu’il rencontrait. Une fort petite céda à ses efforts, et il se trouva dans une cellule au milieu des valets de chambre de Monseigneur, en habits noirs et la chaîne au cou. À son air pressé ces messieurs le crurent mandé par l’évêque et le laissèrent passer. Il fit quelques pas et se trouva dans une immense salle gothique extrêmement sombre, et toute lambrissée de chêne noir ; à l’exception d’une seule, les fenêtres en ogive avaient été murées avec des briques.",
-    categorie: "homme",
-  },
-  {
-    id: 6,
-    img: "./assets/USB_64GB.jpg",
-    nom: "Chapeau class homme",
-    prix: 10,
-    // Description de 30 mots
-    description:
-      "L’humeur hautaine de Julien fut choquée de l’insolence des laquais. Il se mit à parcourir les dortoirs de l’antique abbaye, secouant toutes les portes qu’il rencontrait. Une fort petite céda à ses efforts, et il se trouva dans une cellule au milieu des valets de chambre de Monseigneur, en habits noirs et la chaîne au cou. À son air pressé ces messieurs le crurent mandé par l’évêque et le laissèrent passer. Il fit quelques pas et se trouva dans une immense salle gothique extrêmement sombre, et toute lambrissée de chêne noir ; à l’exception d’une seule, les fenêtres en ogive avaient été murées avec des briques.",
-    categorie: "homme",
-  },
-  {
-    id: 7,
-    img: "./assets/Type_USB_67W.jpg",
-    nom: "Swag hat co",
-    prix: 10,
-    // Description de 30 mots
-    description:
-      "L’humeur hautaine de Julien fut choquée de l’insolence des laquais. Il se mit à parcourir les dortoirs de l’antique abbaye, secouant toutes les portes qu’il rencontrait. Une fort petite céda à ses efforts, et il se trouva dans une cellule au milieu des valets de chambre de Monseigneur, en habits noirs et la chaîne au cou. À son air pressé ces messieurs le crurent mandé par l’évêque et le laissèrent passer. Il fit quelques pas et se trouva dans une immense salle gothique extrêmement sombre, et toute lambrissée de chêne noir ; à l’exception d’une seule, les fenêtres en ogive avaient été murées avec des briques.",
-    categorie: "homme",
-  },
-  {
-    id: 8,
-    img: "./assets/JBL _K73.jpg",
-    nom: "Chapeau uhulu",
-    prix: 4,
-    // Description de 30 mots
-    description:
-      "L’humeur hautaine de Julien fut choquée de l’insolence des laquais. Il se mit à parcourir les dortoirs de l’antique abbaye, secouant toutes les portes qu’il rencontrait. Une fort petite céda à ses efforts, et il se trouva dans une cellule au milieu des valets de chambre de Monseigneur, en habits noirs et la chaîne au cou. À son air pressé ces messieurs le crurent mandé par l’évêque et le laissèrent passer. Il fit quelques pas et se trouva dans une immense salle gothique extrêmement sombre, et toute lambrissée de chêne noir ; à l’exception d’une seule, les fenêtres en ogive avaient été murées avec des briques.",
-    categorie: "homme",
-  },
-  {
-    id: 9,
-    img: "./assets/Chargeur_C_Iphone.jpg",
-    nom: "Chapeau groute",
-    prix: 2,
-    // Description de 30 mots
-    description:
-      "L’humeur hautaine de Julien fut choquée de l’insolence des laquais. Il se mit à parcourir les dortoirs de l’antique abbaye, secouant toutes les portes qu’il rencontrait. Une fort petite céda à ses efforts, et il se trouva dans une cellule au milieu des valets de chambre de Monseigneur, en habits noirs et la chaîne au cou. À son air pressé ces messieurs le crurent mandé par l’évêque et le laissèrent passer. Il fit quelques pas et se trouva dans une immense salle gothique extrêmement sombre, et toute lambrissée de chêne noir ; à l’exception d’une seule, les fenêtres en ogive avaient été murées avec des briques.",
-    categorie: "homme",
-  },
-  {
-    id: 10,
-    img: "./assets/Cable_C-C.jpg",
-    nom: "Hat break fasion",
-    prix: 10,
-    // Description de 30 mots
-    description:
-      "L’humeur hautaine de Julien fut choquée de l’insolence des laquais. Il se mit à parcourir les dortoirs de l’antique abbaye, secouant toutes les portes qu’il rencontrait. Une fort petite céda à ses efforts, et il se trouva dans une cellule au milieu des valets de chambre de Monseigneur, en habits noirs et la chaîne au cou. À son air pressé ces messieurs le crurent mandé par l’évêque et le laissèrent passer. Il fit quelques pas et se trouva dans une immense salle gothique extrêmement sombre, et toute lambrissée de chêne noir ; à l’exception d’une seule, les fenêtres en ogive avaient été murées avec des briques.",
-    categorie: "homme",
-  },
-  {
-    id: 11,
-    img: "./assets/Filaire_Type C.jpg",
-    nom: "Jackpot hat for man",
-    prix: 10,
-    // Description de 30 mots
-    description:
-      "L’humeur hautaine de Julien fut hoquée de l’insolence des laquais. Il se mit à parcourir les dortoirs de l’antique abbaye, secouant toutes les portes qu’il rencontrait. Une fort petite céda à ses efforts, et il se trouva dans une cellule au milieu des valets de chambre de Monseigneur, en habits noirs et la chaîne au cou. À son air pressé ces messieurs le crurent mandé par l’évêque et le laissèrent passer. Il fit quelques pas et se trouva dans une immense salle gothique extrêmement sombre, et toute lambrissée de chêne noir ; à l’exception d’une seule, les fenêtres en ogive avaient été murées avec des briques.",
-    categorie: "homme",
-  },
-  {
-    id: 12,
-    img: "./assets/Chargeur_C_Iphone.jpg",
-    nom: "Men's swag style",
-    prix: 10,
-    // Description de 30 mots
-    description:
-      "L’humeur hautaine de Julien fut choquée de l’insolence des laquais. Il se mit à parcourir les dortoirs de l’antique abbaye, secouant toutes les portes qu’il rencontrait. Une fort petite céda à ses efforts, et il se trouva dans une cellule au milieu des valets de chambre de Monseigneur, en habits noirs et la chaîne au cou. À son air pressé ces messieurs le crurent mandé par l’évêque et le laissèrent passer. Il fit quelques pas et se trouva dans une immense salle gothique extrêmement sombre, et toute lambrissée de chêne noir ; à l’exception d’une seule, les fenêtres en ogive avaient été murées avec des briques.",
-    categorie: "homme",
-  },
-    {
-    id: 12,
-    img: "./assets/Souris_Chargable.jpg",
-    nom: "Men's swag style",
-    prix: 10,
-    // Description de 30 mots
-    description:
-      "L’humeur hautaine de Julien fut choquée de l’insolence des laquais. Il se mit à parcourir les dortoirs de l’antique abbaye, secouant toutes les portes qu’il rencontrait. Une fort petite céda à ses efforts, et il se trouva dans une cellule au milieu des valets de chambre de Monseigneur, en habits noirs et la chaîne au cou. À son air pressé ces messieurs le crurent mandé par l’évêque et le laissèrent passer. Il fit quelques pas et se trouva dans une immense salle gothique extrêmement sombre, et toute lambrissée de chêne noir ; à l’exception d’une seule, les fenêtres en ogive avaient été murées avec des briques.",
-    categorie: "homme",
-  },
-    {
-    id: 12,
-    img: "./assets/Support Ordinateur.jpg",
-    nom: "Men's swag style",
-    prix: 10,
-    // Description de 30 mots
-    description:
-      "L’humeur hautaine de Julien fut choquée de l’insolence des laquais. Il se mit à parcourir les dortoirs de l’antique abbaye, secouant toutes les portes qu’il rencontrait. Une fort petite céda à ses efforts, et il se trouva dans une cellule au milieu des valets de chambre de Monseigneur, en habits noirs et la chaîne au cou. À son air pressé ces messieurs le crurent mandé par l’évêque et le laissèrent passer. Il fit quelques pas et se trouva dans une immense salle gothique extrêmement sombre, et toute lambrissée de chêne noir ; à l’exception d’une seule, les fenêtres en ogive avaient été murées avec des briques.",
-    categorie: "homme",
-  },
-  {
-    id: 13,
-    img: "./assets/K30.jpg",
-    nom: "Men's swag style",
-    prix: 10,
-    // Description de 30 mots
-    description:
-      "L’humeur hautaine de Julien fut choquée de l’insolence des laquais. Il se mit à parcourir les dortoirs de l’antique abbaye, secouant toutes les portes qu’il rencontrait. Une fort petite céda à ses efforts, et il se trouva dans une cellule au milieu des valets de chambre de Monseigneur, en habits noirs et la chaîne au cou. À son air pressé ces messieurs le crurent mandé par l’évêque et le laissèrent passer. Il fit quelques pas et se trouva dans une immense salle gothique extrêmement sombre, et toute lambrissée de chêne noir ; à l’exception d’une seule, les fenêtres en ogive avaient été murées avec des briques.",
-    categorie: "homme",
-  },
+   
+  
 ];
